664b13400f4e8c734ce6f93f9dd4927c0e424368
blackcode2 <doky03115@gmail.com>
2025-08-23 05:13:25 +0900

    [feat]: Alamofire package & code for SSE

---
+++ SafeVison/Views/HomeView.swift
@@ -0,0 +1,19 @@
+//
+//  HomeView.swift
+//  SafeVision
+//
+//  Created by KimDogyung on 8/23/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+
+#Preview {
+    HomeView()
+}
