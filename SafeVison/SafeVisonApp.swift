168306db1ccd1e1270687944cf41c23688086fda
blackcode2 <doky03115@gmail.com>
2025-08-23 02:01:40 +0900

    Initial Commit

---
+++ SafeVison/SafeVisonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeVisonApp.swift
+//  SafeVison
+//
+//  Created by KimDogyung on 8/23/25.
+//
+
+import SwiftUI
+
+@main
+struct SafeVisonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
