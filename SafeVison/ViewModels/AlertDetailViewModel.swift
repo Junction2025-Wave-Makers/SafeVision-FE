0b02262dfc9ed90b1946e0d345753c7e79f30774
blackcode2 <doky03115@gmail.com>
2025-08-23 22:00:08 +0900

    alert detail View 완성

--- SafeVison/ViewModels/AlertDetailViewModel.swift
+++ SafeVison/ViewModels/AlertDetailViewModel.swift
@@ -12,9 +12,30 @@ class AlertDetailViewModel: ObservableObject {
     @Published var detectPlayer: AVPlayer? = nil
     @Published var streamPlayer: AVPlayer? = nil
     
-    func loadVideo(from url: URL) {
+    func loadVideo(byFileName name: String) {
+       // 파일 이름과 확장자 분리
+        let components = name.split(separator: ".").map(String.init)
+        guard components.count == 2,
+              let fileName = components.first,
+              let fileExtension = components.last else {
+            print("잘못된 파일 이름 형식입니다: \(name)")
+            return
+        }
+        
+        // 메인 번들에서 비디오 파일의 URL을 찾기
+        guard let url = Bundle.main.url(forResource: fileName, withExtension: fileExtension) else {
+            print("메인 번들에서 비디오 파일을 찾을 수 없습니다: \(name)")
+            return
+        }
+        
+        // 찾은 URL로 AVPlayer를 생성합니다.
         detectPlayer = AVPlayer(url: url)
     }
+    
+    
+//    func loadVideo(from url: URL) {
+//        detectPlayer = AVPlayer(url: url)
+//    }
     
 //    func loadVideo(from data: Data) {
 //        let tempDir = FileManager.default.temporaryDirectory
